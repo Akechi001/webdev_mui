17b5185613c360c5315b4c165edb9a752b8d3cba
Akechi001 <enricoarianthous@gmail.com>
2023-12-02 12:50:43 +0800

    Heaadbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import {Stack, Grid, Box, Typography, FormControlLabel, Switch} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Stack direction={"column"}>
+          <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "10vh",
+                marginTop: "30px"
+            }}>
+                <Stack direction="row" spacing={100}>
+                    <Typography fontSize={"30px"} fontWeight={"bold"}>
+                        Social Media Dashboard
+                        <Typography color={"grey"} fontWeight={"bold"}>
+                            {""}
+                            Total Followers : 23,004
+                        </Typography>
+                    </Typography>
+                    <FormControlLabel
+                        value="start"
+                        control={<Switch />}
+                        label="Dark Mode"
+                        labelPlacement={"start"}
+
+                        sx={{
+                            "& .MuiTypography-root":{fontWeight: "bold", color: "grey"},
+                            "& .MuiSwitch-root": {
+                                padding:"5px",
+                                width:"60px",
+                            },
+                            "& .MuiSwitch-track":{
+                                borderRadius:"13px",
+                            },
+
+                        }}
+                    />
+                </Stack>
+          </Box>
+
+          </Stack>
+      );
 }
 
 export default App;
